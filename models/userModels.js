50b23a072a3a5e1cf3a92e76cc37af9fe7bba201
Tanushree227 <tanushreedas813@gmail.com>
2024-01-12 11:38:21 +0530

    user model, routes, controller setup

---
+++ models/userModels.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname: { 
+        type: String, 
+        required: [true, 'Name is Required'] 
+    },
+    email: {
+       type: String,
+       required: [true, 'Email is Required'] 
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone is Required'] 
+     },
+     password: {
+        type: String,
+        required: [true, 'Password is Required'] 
+     },
+     c_password: {
+        type: String,
+        required: [true, 'Confirm Password is Required'] 
+     },
+})
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel;
